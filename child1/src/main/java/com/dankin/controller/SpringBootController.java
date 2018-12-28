d20b3a6776816865ac698b262af4041e47ae006f
duankang <duankang@kingtopware.com>
2018-12-28 17:38:28 +0800

    springboot项目

---
+++ child1/src/main/java/com/dankin/controller/SpringBootController.java
@@ -0,0 +1,11 @@
+package com.dankin.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
